0e8c4d749aa673718a71cfe67f266e6acdc4fb86
keneikevin <kkevinkenei@gmail.com>
2022-03-05 20:36:34 +0300

    Nested RecyclerView

--- app/src/main/java/com/example/nestedscroll/DescActivity.kt
+++ app/src/main/java/com/example/nestedscroll/DescActivity.kt
@@ -1,9 +1,11 @@
 package com.example.nestedscroll
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_desc.*
+
 
 class DescActivity : AppCompatActivity() {
     lateinit var viewModel: MainActivityViewModel
@@ -29,6 +31,10 @@ class DescActivity : AppCompatActivity() {
 
 
         Glide.with(imageview).load(data?.url).into(imageview)
+        button.setOnClickListener {
+           //Todo add name and price to room database
+
+        }
     }
 
 }
